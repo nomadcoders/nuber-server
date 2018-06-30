821f576a77e0c6bd21823b012310acd61d0b6ad9
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-06-30 14:11:58 +0300

    #1.7 API and Schema Structue part Two

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import cors from "cors";
 import { GraphQLServer } from "graphql-yoga";
 import helmet from "helmet";
 import logger from "morgan";
+import schema from "./schema";
 
 class App {
   public app: GraphQLServer;
   constructor() {
-    this.app = new GraphQLServer({});
+    this.app = new GraphQLServer({
+      schema
+    });
     this.middlewares();
   }
   private middlewares = (): void => {
--- src/schema.ts
+++ src/schema.ts
@@ -2,3 +2,21 @@ import { GraphQLSchema } from "graphql";
 import { makeExecutableSchema } from "graphql-tools";
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
 import path from "path";
+
+const allTypes: GraphQLSchema[] = fileLoader(
+  path.join(__dirname, "./api/**/*.graphql")
+);
+
+const allResolvers: string[] = fileLoader(
+  path.join(__dirname, "./api/**/*.resolvers.*")
+);
+
+const mergedTypes = mergeTypes(allTypes);
+const mergedResolvers = mergeResolvers(allResolvers);
+
+const schema = makeExecutableSchema({
+  typeDefs: mergedTypes,
+  resolvers: mergedResolvers
+});
+
+export default schema;
