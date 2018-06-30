36c25886a73dd4c5fec2e34065d001be4d9e081c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-06-30 16:44:17 +0300

    #1.14 User Entity part Two

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -40,6 +40,28 @@ class User extends BaseEntity {
   @Column({ type: "text" })
   profilePhoto: string;
 
+  @Column({ type: "boolean", default: false })
+  isDriving: boolean;
+
+  @Column({ type: "boolean", default: false })
+  isRiding: boolean;
+
+  @Column({ type: "boolean", default: false })
+  isTaken: boolean;
+
+  @Column({ type: "double precision", default: 0 })
+  lastLng: number;
+
+  @Column({ type: "double precision", default: 0 })
+  lastLat: number;
+
+  @Column({ type: "double precision", default: 0 })
+  lastOrientation: number;
+
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
   @CreateDateColumn() createdAt: string;
   @UpdateDateColumn() updatedAt: string;
 }
