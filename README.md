95f678a0025d5b62d5524e2970ee002b108f7b7d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-02 18:42:07 +0300

    #1.40 EmailSignUp Resolver

--- README.md
+++ README.md
@@ -9,15 +9,20 @@ Server for the (N)Uber Clone Course on Nomad Academy. GraphQL, Typescript, NodeJ
 - [x] Sign In / Sign Up with Facebook
 - [x] Sign In with Email
 - [x] Start Phone Number Verification
-- [ ] Complete Phone Number Verification
+- [x] Complete Phone Number Verification
 - [ ] Sign Up with Email
+
+---
+
+### Authentication:
+
+- [ ] Generate JWT
+- [ ] Verifiy JWT
 
 ---
 
 ### Private Resolvers:
 
-- [ ] Generate JWT
-- [ ] Verifiy JWT
 - [ ] Verify Email
 - [ ] Get my Profile
 - [ ] Update my Profile
---
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -0,0 +1,42 @@
+import User from "../../../entities/User";
+import {
+  EmailSignUpMutationArgs,
+  EmailSignUpResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EmailSignUp: async (
+      _,
+      args: EmailSignUpMutationArgs
+    ): Promise<EmailSignUpResponse> => {
+      const { email } = args;
+      try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return {
+            ok: false,
+            error: "You should log in instead",
+            token: null
+          };
+        } else {
+          const newUser = await User.create({ ...args }).save();
+          return {
+            ok: true,
+            error: null,
+            token: "Coming soon!"
+          };
+        }
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
