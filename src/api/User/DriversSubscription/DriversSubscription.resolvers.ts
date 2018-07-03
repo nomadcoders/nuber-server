31b154ad159e5fb2d22bbd8793b3ea75cd1836c1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-03 18:39:19 +0300

    #1.70 Filtering Subscription Messages

--- src/api/User/DriversSubscription/DriversSubscription.resolvers.ts
+++ src/api/User/DriversSubscription/DriversSubscription.resolvers.ts
@@ -1,9 +1,28 @@
+import { withFilter } from "graphql-yoga";
+import User from "../../../entities/User";
+
 const resolvers = {
   Subscription: {
     DriversSubscription: {
-      subscribe: (_, __, { pubSub }) => {
-        return pubSub.asyncIterator("driverUpdate");
-      }
+      subscribe: withFilter(
+        (_, __, { pubSub }) => pubSub.asyncIterator("driverUpdate"),
+        (payload, _, { context }) => {
+          const user: User = context.currentUser;
+          const {
+            DriversSubscription: {
+              lastLat: driverLastLat,
+              lastLng: driverLastLng
+            }
+          } = payload;
+          const { lastLat: userLastLat, lastLng: userLastLng } = user;
+          return (
+            driverLastLat >= userLastLat - 0.05 &&
+            driverLastLat <= userLastLat + 0.05 &&
+            driverLastLng >= userLastLng - 0.05 &&
+            driverLastLng <= userLastLng + 0.05
+          );
+        }
+      )
     }
   }
 };
