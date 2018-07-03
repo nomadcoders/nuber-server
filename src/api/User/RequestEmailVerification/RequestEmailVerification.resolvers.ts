1a640e53e49441f4fdf1cd6fbe9f6a872aa3d837
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-03 13:42:18 +0300

    #1.53 RequestEmailVerification Resolver

---
+++ src/api/User/RequestEmailVerification/RequestEmailVerification.resolvers.ts
@@ -0,0 +1,47 @@
+import User from "../../../entities/User";
+import Verification from "../../../entities/Verification";
+import { RequestEmailVerificationResponse } from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import { sendVerificationEmail } from "../../../utils/sendEmail";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    RequestEmailVerification: privateResolver(
+      async (_, __, { req }): Promise<RequestEmailVerificationResponse> => {
+        const user: User = req.user;
+        if (user.email) {
+          try {
+            const oldVerification = await Verification.findOne({
+              payload: user.email
+            });
+            if (oldVerification) {
+              oldVerification.remove();
+            }
+            const newVerification = await Verification.create({
+              payload: user.email,
+              target: "EMAIL"
+            }).save();
+            await sendVerificationEmail(user.fullName, newVerification.key);
+            return {
+              ok: true,
+              error: null
+            };
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: "Your user has no email to verify"
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
