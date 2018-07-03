47c803df1abe59d375dcc2f2c755800d9eabb955
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-03 13:48:52 +0300

    #1.54 CompleteEmailVerification Resolver

---
+++ src/api/User/CompleteEmailVerification/CompleteEmailVerification.resolvers.ts
@@ -0,0 +1,56 @@
+import User from "../../../entities/User";
+import Verification from "../../../entities/Verification";
+import {
+  CompleteEmailVerificationMutationArgs,
+  CompleteEmailVerificationResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    CompleteEmailVerification: privateResolver(
+      async (
+        _,
+        args: CompleteEmailVerificationMutationArgs,
+        { req }
+      ): Promise<CompleteEmailVerificationResponse> => {
+        const user: User = req.user;
+        const { key } = args;
+        if (user.email) {
+          try {
+            const verification = await Verification.findOne({
+              key,
+              payload: user.email
+            });
+            if (verification) {
+              user.verifiedEmail = true;
+              user.save();
+              return {
+                ok: true,
+                error: null
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Cant verify email"
+              };
+            }
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message
+            };
+          }
+        } else {
+          return {
+            ok: false,
+            error: "No email to verify"
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
