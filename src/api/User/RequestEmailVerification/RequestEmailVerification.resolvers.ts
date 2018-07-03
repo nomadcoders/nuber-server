25148eb5e32fe0a8088d6c96b86ae633d6ff937a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-03 13:52:20 +0300

    #1.55 Testing Email Verification Resolvers

--- src/api/User/RequestEmailVerification/RequestEmailVerification.resolvers.ts
+++ src/api/User/RequestEmailVerification/RequestEmailVerification.resolvers.ts
@@ -10,7 +10,7 @@ const resolvers: Resolvers = {
     RequestEmailVerification: privateResolver(
       async (_, __, { req }): Promise<RequestEmailVerificationResponse> => {
         const user: User = req.user;
-        if (user.email) {
+        if (user.email && !user.verifiedEmail) {
           try {
             const oldVerification = await Verification.findOne({
               payload: user.email
