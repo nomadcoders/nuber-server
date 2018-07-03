23ce320b8ba131e55678876c01465a99252ee65a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-03 17:43:35 +0300

    #1.67 DriversSubscription part Two

--- src/api/User/ReportMovement/ReportMovement.resolvers.ts
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -13,12 +13,13 @@ const resolvers: Resolvers = {
       async (
         _,
         args: ReportMovementMutationArgs,
-        { req }
+        { req, pubSub }
       ): Promise<ReportMovementResponse> => {
         const user: User = req.user;
         const notNull = cleanNullArgs(args);
         try {
           await User.update({ id: user.id }, { ...notNull });
+          pubSub.publish("driverUpdate", { DriversSubscription: user });
           return {
             ok: true,
             error: null
