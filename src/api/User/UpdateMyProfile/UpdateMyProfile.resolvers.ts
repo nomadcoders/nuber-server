a91f622e519c6f0fd8dd849891ccebe7ffc55050
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-03 14:14:34 +0300

    #1.57 UpdateMyProfile Resolver part Two

--- src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
+++ src/api/User/UpdateMyProfile/UpdateMyProfile.resolvers.ts
@@ -1,14 +1,38 @@
 import User from "../../../entities/User";
-import { UpdateMyProfileMutationArgs } from "../../../types/graph";
+import {
+  UpdateMyProfileMutationArgs,
+  UpdateMyProfileResponse
+} from "../../../types/graph";
 import { Resolvers } from "../../../types/resolvers";
 import privateResolver from "../../../utils/privateResolver";
 
 const resolvers: Resolvers = {
   Mutation: {
     UpdateMyProfile: privateResolver(
-      async (_, args: UpdateMyProfileMutationArgs, { req }) => {
+      async (
+        _,
+        args: UpdateMyProfileMutationArgs,
+        { req }
+      ): Promise<UpdateMyProfileResponse> => {
         const user: User = req.user;
-        await User.update({ id: user.id }, { ...args });
+        const notNull = {};
+        Object.keys(args).forEach(key => {
+          if (args[key] !== null) {
+            notNull[key] = args[key];
+          }
+        });
+        try {
+          await User.update({ id: user.id }, { ...notNull });
+          return {
+            ok: true,
+            error: null
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message
+          };
+        }
       }
     )
   }
