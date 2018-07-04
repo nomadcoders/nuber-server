d77c7fb8c230d89bf25a002d68b7634a43fda1f6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-04 17:29:31 +0300

    #1.75 Testing the NearbyRideSubscription

--- README.md
+++ README.md
@@ -38,6 +38,7 @@ Server for the (N)Uber Clone Course on Nomad Academy. GraphQL, Typescript, NodeJ
 - [x] Request a Ride
 - [x] Get Nearby Ride Requests
 - [x] Subscribe to Nearby Ride Requests
+- [ ] Update Ride Status
 - [ ] Subscribe to Ride Status
 - [ ] Get Chat Room Messages
 - [ ] Subscribe to Chat Room Messages
--- src/api/Ride/RequestRide/RequestRide.resolvers.ts
+++ src/api/Ride/RequestRide/RequestRide.resolvers.ts
@@ -16,18 +16,28 @@ const resolvers: Resolvers = {
         { req, pubSub }
       ): Promise<RequestRideResponse> => {
         const user: User = req.user;
-        try {
-          const ride = await Ride.create({ ...args, passenger: user }).save();
-          pubSub.publish("rideRequest", { NearbyRideSubscription: ride });
-          return {
-            ok: true,
-            error: null,
-            ride
-          };
-        } catch (error) {
+        if (!user.isRiding) {
+          try {
+            const ride = await Ride.create({ ...args, passenger: user }).save();
+            pubSub.publish("rideRequest", { NearbyRideSubscription: ride });
+            user.isRiding = true;
+            user.save();
+            return {
+              ok: true,
+              error: null,
+              ride
+            };
+          } catch (error) {
+            return {
+              ok: false,
+              error: error.message,
+              ride: null
+            };
+          }
+        } else {
           return {
             ok: false,
-            error: error.message,
+            error: "You can't request two rides",
             ride: null
           };
         }
