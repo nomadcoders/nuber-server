e2673cc2243d764f927d10d952ec91b9ca70a0a6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-04 19:32:22 +0300

    #1.85 SendChatMessage Resolver

--- README.md
+++ README.md
@@ -43,8 +43,8 @@ Server for the (N)Uber Clone Course on Nomad Academy. GraphQL, Typescript, NodeJ
 - [x] Subscribe to Ride Status
 - [x] Create a Chat Room
 - [x] Get Chat Room Messages
+- [x] Send a Chat Message
 - [ ] Subscribe to Chat Room Messages
-- [ ] Send a Chat Message
 
 ## Code Challenge
 
---
+++ src/api/Chat/SendChatMessage/SendChatMessage.resolvers.ts
@@ -0,0 +1,60 @@
+import Chat from "../../../entities/Chat";
+import Message from "../../../entities/Message";
+import User from "../../../entities/User";
+import {
+  SendChatMessageMutationArgs,
+  SendChatMessageResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    SendChatMessage: privateResolver(
+      async (
+        _,
+        args: SendChatMessageMutationArgs,
+        { req }
+      ): Promise<SendChatMessageResponse> => {
+        const user: User = req.user;
+        try {
+          const chat = await Chat.findOne({ id: args.chatId });
+          if (chat) {
+            if (chat.passengerId === user.id || chat.driverId === user.id) {
+              const message = await Message.create({
+                text: args.text,
+                chat,
+                user
+              }).save();
+              return {
+                ok: true,
+                error: null,
+                message
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Unauthorized",
+                message: null
+              };
+            }
+          } else {
+            return {
+              ok: false,
+              error: "Chat not found",
+              message: null
+            };
+          }
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            message: null
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
