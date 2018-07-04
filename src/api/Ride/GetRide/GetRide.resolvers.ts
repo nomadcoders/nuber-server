b869b02e620a4c7c7850e744ee3d5a3b055b7d59
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-07-04 17:59:49 +0300

    #1.78 GetRide Resolver

---
+++ src/api/Ride/GetRide/GetRide.resolvers.ts
@@ -0,0 +1,50 @@
+import Ride from "../../../entities/Ride";
+import User from "../../../entities/User";
+import { GetRideQueryArgs, GetRideResponse } from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+  Query: {
+    GetRide: privateResolver(
+      async (_, args: GetRideQueryArgs, { req }): Promise<GetRideResponse> => {
+        const user: User = req.user;
+        try {
+          const ride = await Ride.findOne({
+            id: args.rideId
+          });
+
+          if (ride) {
+            if (ride.passengerId === user.id || ride.driverId === user.id) {
+              return {
+                ok: true,
+                error: null,
+                ride
+              };
+            } else {
+              return {
+                ok: false,
+                error: "Not Authorized",
+                ride: null
+              };
+            }
+          } else {
+            return {
+              ok: false,
+              error: "Ride not found",
+              ride: null
+            };
+          }
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+            ride: null
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
--- src/entities/Ride.ts
+++ src/entities/Ride.ts
@@ -17,7 +17,7 @@ class Ride extends BaseEntity {
   @Column({
     type: "text",
     enum: ["ACCEPTED", "FINISHED", "CANCELED", "REQUESTING", "ONROUTE"],
-    default: "ACCEPTED"
+    default: "REQUESTING"
   })
   status: rideStatus;
 
@@ -48,8 +48,14 @@ class Ride extends BaseEntity {
   @Column({ type: "text" })
   duration: string;
 
+  @Column({ nullable: true })
+  passengerId: number;
+
   @ManyToOne(type => User, user => user.ridesAsPassenger)
   passenger: User;
+
+  @Column({ nullable: true })
+  driverId: number;
 
   @ManyToOne(type => User, user => user.ridesAsDriver, { nullable: true })
   driver: User;
