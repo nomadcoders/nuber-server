675e8a80bc1735d21ced540e573dae1ece71058d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-08-14 21:48:46 +0300

    GetNearbyRide Bugfix

--- src/api/Ride/GetNearbyRide/GetNearbyRide.resolvers.ts
+++ src/api/Ride/GetNearbyRide/GetNearbyRide.resolvers.ts
@@ -13,11 +13,14 @@ const resolvers: Resolvers = {
         if (user.isDriving) {
           const { lastLat, lastLng } = user;
           try {
-            const ride = await getRepository(Ride).findOne({
-              status: "REQUESTING",
-              pickUpLat: Between(lastLat - 0.05, lastLat + 0.05),
-              pickUpLng: Between(lastLng - 0.05, lastLng + 0.05)
-            });
+            const ride = await getRepository(Ride).findOne(
+              {
+                status: "REQUESTING",
+                pickUpLat: Between(lastLat - 0.05, lastLat + 0.05),
+                pickUpLng: Between(lastLng - 0.05, lastLng + 0.05)
+              },
+              { relations: ["passenger"] }
+            );
             if (ride) {
               return {
                 ok: true,
